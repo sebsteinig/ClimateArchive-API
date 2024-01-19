e0aeb12e62ce1e2ea8392ee0758acf23097f6786
sebsteinig <seb.steinig@gmail.com>
2024-01-19 17:12:47 +0000

    added cmip6 route and error handling

--- app.py
+++ app.py
@@ -1,19 +1,91 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-from get_model_data import extract_annual_data_UM
+from get_model_data import extract_annual_data_UM, extract_ts_data_cmip
 
-@app.route('/get_model_data', methods=['POST'])
-def get_model_data():
+#########################################################################################
+# BRIDGE annual mean climatology
+#########################################################################################
+
+@app.route('/get_mean_data_bridge', methods=['POST'])
+def get_mean_data_bridge():
     data = request.json
+
+    # check that the request contains the expected keys
+    if not all(key in data for key in ['model_ids', 'locations', 'variable']):
+        return jsonify({'error': 'Missing data. Expected keys are: model_ids, locations and variable'}), 400
+
     model_ids = data['model_ids']
     locations = data['locations']
     variable = data['variable']
 
-    results = extract_annual_data_UM(model_ids, locations, variable)
-    # Convert results to a serializable format if necessary
-    return jsonify(results)
+    # check for correct API call
+    # validate model_ids and locations
+    if not isinstance(model_ids, list) or not all(isinstance(id, str) for id in model_ids):
+        return jsonify({'error': 'model_ids should be a list of strings'}), 400
+    if not isinstance(locations, list) or not all(isinstance(loc, (list, tuple)) and len(loc) == 2 for loc in locations):
+        return jsonify({'error': 'locations should be a list of tuples'}), 400
 
+    # check if the length of model_ids and locations is the same
+    if len(model_ids) != len(locations):
+        return jsonify({'error': 'model_ids and locations must have the same length'}), 400
+
+    # validate variable: should be a string
+    if not isinstance(variable, str):
+        return jsonify({'error': 'variable should be a string'}), 400
+    
+    # validate that known variable was requested
+    if variable not in ['tas', 'pr']:
+        return jsonify({'error': f'Unknown variable: {variable}. Currently supported variables are: tas and pr'}), 400
+
+    # try to extract the data
+    try:
+        results = extract_annual_data_UM(model_ids, locations, variable)
+        return jsonify(results)
+    except ValueError as ve:
+        return jsonify({'error': str(ve)}), 400  # Bad request
+    except RuntimeError as re:
+        return jsonify({'error': str(re)}), 500  # Internal server error
+
+#########################################################################################
+# CMIP6 annual mean scneario timeseries
+#########################################################################################
+    
+@app.route('/get_ts_data_cmip', methods=['POST'])
+def get_ts_data_cmip():
+    data = request.json
+
+    # check that the request contains the expected keys
+    if not all(key in data for key in ['model_id', 'location', 'variable']):
+        return jsonify({'error': 'Missing data. Expected keys are: model_id, location and variable'}), 400
+
+    model_id = data['model_id']
+    location = data['location']
+    variable = data['variable']
+
+    # check for correct API call
+    # validate model_ids, locations and variable
+    if not isinstance(model_id, str):
+        return jsonify({'error': 'model_id should be a single string'}), 400
+    if not model_id in ['ssp126', 'ssp245', 'ssp370', 'ssp585']:
+        return jsonify({'error': f'Unknown model_id: {model_id}. Currently supported model_ids are: ssp126, ssp245, ssp370 and ssp585'}), 400
+    if not isinstance(location, list) or not len(location) == 2:
+        return jsonify({'error': 'location should be a list of length 2'}), 400
+    if not isinstance(variable, str):
+        return jsonify({'error': 'variable should be a string'}), 400
+    
+    # validate that known variable was requested
+    if variable not in ['tas', 'pr']:
+        return jsonify({'error': f'Unknown variable: {variable}. Currently supported variables are: tas and pr'}), 400
+
+    # try to extract the data
+    try:
+        results = extract_ts_data_cmip(model_id, location, variable)
+        return jsonify(results)
+    except ValueError as ve:
+        return jsonify({'error': str(ve)}), 400  # Bad request
+    except RuntimeError as re:
+        return jsonify({'error': str(re)}), 500  # Internal server error
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
--- get_model_data.py
+++ get_model_data.py
@@ -1,21 +1,68 @@
 import xarray as xr
+import logging
 
 # the actual model data (netcdf files) live outside the Docker container
 # when starting the container, we map the directory containing the model data
 # as a volume to '/data' inside the container
-data_dir = '/data'
+# data_dir = '/data'
+data_dir = '/Volumes/WD_Elements/data/ClimateArchive'
 
-def extract_annual_data_UM(model_ids, locations, variable):
+# define the mapping of user input variable names to dataset variable names
+variable_name_mapping_UM = {
+    'tas': 'temp_mm_1_5m',
+    'pr': 'precip_mm_srf'
+}
+
+def extract_annual_data_UM(model_ids, locations, user_variable):
     results = []
+    # translate user variable name to UM variable name
+    variable = variable_name_mapping_UM.get(user_variable)
     for model_id, (lat, lon) in zip(model_ids, locations):
-        # Load the NetCDF file for the model (this is just an example path)
-        ds = xr.open_dataset(f'{data_dir}/{model_id}/climate/{model_id}a.pdclann.nc', decode_times=False)
-        # Extract data for the specified location
-        data = ds[variable].sel(latitude=lat, longitude=lon, method='nearest').squeeze().values.tolist()
+        try:
+            # Load the NetCDF file for the model (this is just an example path)
+            ds = xr.open_dataset(f'{data_dir}/bridge_hadcm3/{model_id}/climate/{model_id}a.pdclann.nc', decode_times=False)
+            # Extract data for the specified location
+            data = ds[variable].sel(latitude=lat, longitude=lon, method='nearest').squeeze().values.tolist()
 
-        if variable == 'temp_mm_1_5m':
-            # Convert from Kelvin to Celsius
-            data = round(data - 273.15, 2) 
-            
-        results.append(data)
+            if variable == 'temp_mm_1_5m':
+                # Convert from Kelvin to Celsius
+                data = round(data - 273.15, 2) 
+            elif variable == 'precip_mm_srf':
+                # Convert from kg/m^2/s to mm/day
+                data = round(data * 86400, 2)
+                
+            results.append(data)
+
+        except FileNotFoundError:
+            raise ValueError(f"File not found for model_id: {model_id}")
+        except KeyError:
+            raise ValueError(f"Variable '{variable}' not found in dataset for model_id: {model_id}")
+        except Exception as e:
+            raise RuntimeError(f"Error processing data for model_id {model_id}: {str(e)}")
+        
     return results
+
+def extract_ts_data_cmip(model_id, location, user_variable):
+    # standard CMIP6 variable names
+    variable = user_variable
+    try:
+        # Load the NetCDF file for the model (this is just an example path)
+        ds = xr.open_dataset(f'{data_dir}/cmip6/{variable}_mon_mod_{model_id}_192_ave.ym.nc', decode_times=False)
+        # Extract data for the specified location
+        data = ds[variable].sel(lat=location[0], lon=location[1], method='nearest').squeeze().values.tolist()
+
+        if variable == 'tas':
+            # Convert from Kelvin to Celsius for each element in the list
+            data = [round(datum - 273.15, 2) for datum in data]
+        elif variable == 'pr':
+            # Convert from kg/m^2/s to mm/day for each element in the list
+            data = [round(datum * 86400, 2) for datum in data]
+    
+    except FileNotFoundError:
+        raise ValueError(f"File not found for model_id: {model_id}")
+    except KeyError:
+        raise ValueError(f"Variable '{variable}' not found in dataset for model_id: {model_id}")
+    except Exception as e:
+        raise RuntimeError(f"Error processing data for model_id {model_id}: {str(e)}")
+        
+    return data
